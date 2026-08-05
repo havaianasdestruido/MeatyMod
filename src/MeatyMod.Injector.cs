6d37cc98c8030ad92fa81321dbc425780e5ee5aa
UltimateQuack <patoflamejantetv@gmail.com>
2026-08-05 16:36:52 -0300

    feat(injector): add IL patching skeleton

---
+++ src/MeatyMod.Injector.cs
@@ -0,0 +1,12 @@
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+
+namespace MeatyMod.Injector;
+
+public class Injector
+{
+    public void Patch(string assemblyPath)
+    {
+        // TODO: Implement IL patching
+    }
+}
